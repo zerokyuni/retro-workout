968cbc649fc30605ee3820aac521fe90c2411f63
zerokyuni <zzang941115@gmail.com>
2026-07-06 17:39:45 +0900

    Update presets.js

--- preset/presets.js
+++ preset/presets.js
@@ -38,7 +38,27 @@ window.VCR_PRESETS = [
             { name: "SHADOW BOXING", gif: "./gif/boxing.gif" }
         ]
     }
-    
+
+    , {
+        id: "CH04",
+        type: "preset",
+        name: "LEG CORE",
+        work: 5,
+        rest: 3,
+        sets: 9,
+        gif: "./gif/ch04.gif",
+        playlist: [
+            { name: "BUTTERFLY", gif: "./gif/4butterfly.gif" },
+            { name: "GLUTE BRIDGE", gif: "./gif/4glu.bridge.gif" },
+            { name: "CAT STRETCH", gif: "./gif/4cat.strch.gif" },
+            { name: "PELVIC TILT", gif: "./gif/4pelv.tilt.gif" },
+            { name: "CHILD POSE", gif: "./gif/4child.p.gif" },
+            { name: "FROG CRUNCH", gif: "./gif/4frog.crch.gif" },
+            { name: "BRIDGE HIP.ABD", gif: "./gif/4bridge_hip.abd.gif" },
+            { name: "BENT LEG SIDE.K L", gif: "./gif/4bt.leg_sideK.gif" },
+            { name: "BENT LEG SIDE.K R", gif: "./gif/4bt.leg_sideK.gif" }
+        ]
+    }
     /* 새로운 프리셋을 추가하려면 아래 형식을 복사해서 붙여넣으세요!
     , {
         id: "CH04",
